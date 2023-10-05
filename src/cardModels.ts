eacac576c4ae27566deedef3bb902ef726d2f55e
Lukas Giger <lukas.giger@axonivy.com>
2023-10-05 11:47:14 +0200

    initial commit

---
+++ src/cardModels.ts
@@ -0,0 +1,15 @@
+/**
+ * Adaptive Card data model. Properties can be referenced in an adaptive card via the `${var}`
+ * Adaptive Card syntax.
+ */
+export interface NewTask {
+  kind: string;
+  name: string;
+  description: string;
+  start: string;
+  expiry: string;
+  activator: string,
+  priority: string;
+  state: string;
+  link: string;
+}
